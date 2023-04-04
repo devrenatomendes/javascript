4bf73d98a0c4a097fcf8874a78296dc7602c5930
devrenatomendes <122327245+devrenatomendes@users.noreply.github.com>
2023-04-04 15:20:08 -0300

    Create 07, 08, 09, 10

--- exercicios/ex007.js
+++ exercicios/ex007.js
@@ -1,1 +1,8 @@
-console.log('O console fech')
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log('Você não ultrapassou a velocidade permitida')
+}
+console.log('Dirija sempre usando cinto de segurança')
