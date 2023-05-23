f136745735440b13a4814c2bd1c1af2adf9020d2
devrenatomendes <remendes99@gmail.com>
2023-05-23 12:09:46 -0300

    Update script.js

--- exercicios/aula-13/ex015/script.js
+++ exercicios/aula-13/ex015/script.js
@@ -1,12 +1,13 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fAno = document.getElementById('txtano')
+    var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fAno.valeu.lenght == 0 || fAno.value > ano) {
+    if (fano.valeu.lenght == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
-        
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
     }
 }
